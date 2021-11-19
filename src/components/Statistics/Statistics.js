bb67a43f7a38d8438eee8679c1a3771c6d23db68
Ilja <tadden12@gmail.com>
2021-11-19 17:54:10 +0200

    refactoring

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,78 +1,79 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Options from '../Options';
 import Notification from '../Notification';
 import s from './Statistic.module.css'
 
+export default function Statistics() {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
-class Statistics extends Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
+    const options = Object.keys({ good, neutral, bad });
+
+    const totalFeedbacks = () => {
+        const total = good + neutral + bad;
+        return total;
     };
 
-    onBtnClick = option => {
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-    });
-  };
-
-    countTotal = () => {
-        const totalFeedbacks = this.state.good + this.state.neutral + this.state.bad;
-        return totalFeedbacks;
-    };
-
-    countPositiveFeedback = () => {
-        const persantage = (this.state.good / this.countTotal()) * 100;
+    const countPositiveFeedback = () => {
+        const persantage = (good / totalFeedbacks()) * 100;
         return persantage.toFixed();
     };
 
-    render() {
-        const options = Object.keys(this.state);
-        
-        return (
-            <div className={s.container}>
-                <Options
-                    options={options}
-                    onBtnClick={this.onBtnClick}
-                />
+   const onBtnClick = option => {
+    switch (option) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+    return (
+        <div className={s.container}>
+            <Options
+                options={options}
+                onBtnClick={onBtnClick}
+            />
+            <>
+                <h2 className={s.title}>Statistic</h2>
+            </>
+
+            {totalFeedbacks() === 0 ? <Notification /> : (
                 <>
-                    <h2 className={s.title}>Statistic</h2>
+                    <p className={s.text}>
+                        <span className={s.score}>Good:</span>
+                        <span>{good}</span>
+                    </p>
+                    <p className={s.text}>
+                        <span className={s.score}>Neutral:</span>
+                        <span>{neutral}</span>
+                    </p>
+                    <p className={s.text}>
+                        <span className={s.score}>Bad:</span>
+                        <span>{bad}</span>
+                    </p>
+                    <p className={s.text}>
+                        <span className={s.score}>Total:</span>
+                        <span>{totalFeedbacks()}</span>
+                    </p>
+                    <p className={s.text}>
+                        <span className={s.score}>Positive feedback: </span>
+                        <span>{countPositiveFeedback()}%</span>
+                    </p>
+           
                 </>
-
-                {this.countTotal() === 0 ? <Notification/> : (
-                    <>
-                <p className={s.text}>
-                    <span className={s.score}>Good:</span>
-                    <span>{this.state.good}</span>
-                </p>
-                <p className={s.text}>
-                    <span className={s.score}>Neutral:</span>
-                    <span>{this.state.neutral}</span>
-                </p>
-                <p className={s.text}>
-                    <span className={s.score}>Bad:</span>
-                    <span>{this.state.bad}</span>
-                </p>
-                <p className={s.text}>
-                    <span className={s.score}>Total:</span>
-                    <span>{this.countTotal()}</span>
-                </p>
-                <p className={s.text}>
-                    <span className={s.score}>Positive feedback: </span>
-                    <span>{this.countPositiveFeedback()}%</span>
-                </p>
-            
-                    </>
-                    
             )}
-             </div>   
-        );
-    };
-};
-
-
-
-export default Statistics;
+        </div>
+    );
+}
